84c9d8e4989674856921c5cac00e4c6d1b084608
Kunal Srivastav <kunalsrivastavgs2001@gmail.com>
2021-10-13 22:12:51 +0530

    update

---
+++ JS/data.js
@@ -0,0 +1,13 @@
+const data = {
+    movies: [],
+    songs: [],
+    webseries: [
+        [
+            {
+                title: "",
+                iframeTag:
+                    '<iframe width="560" height="315" src="https://www.youtube.com/embed/UwsrzCVZAb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            },
+        ],
+    ],
+};
