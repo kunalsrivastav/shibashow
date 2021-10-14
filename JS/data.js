0659b5419490fe4278b930ffb11c2c69c01b6538
nikhil-gaur457 <nikhil.gaur_cs19@gla.ac.in>
2021-10-14 23:30:43 +0530

    updating data.js with movies data

--- JS/data.js
+++ JS/data.js
@@ -1,13 +1,51 @@
 const data = {
-    movies: [],
+    movies: [{
+            title: "DJ",
+            duration: "2:24",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/OOQlCD5qMPY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        },
+        {
+            title: "2 States",
+            duration: "2:00",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/neB7WugMKT4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Welcome Back",
+            duration: "2:03",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/GhZufxyGO-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Kis Kisko Pyaar Karoon",
+            duration: "2:12",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/kGun5Mx5-oI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Garam Masala",
+            duration: "2:18",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/gxBERAhEU7w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Jodha Akbar",
+            duration: "3:07",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/wgDo0dLm1V4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Border",
+            duration: "3:07",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/glV9AaLTMZU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "LOC Kargil",
+            duration: "4:02",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/diBHXLm052o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+    ],
     songs: [],
     webseries: [
-        [
-            {
-                title: "",
-                iframeTag:
-                    '<iframe width="560" height="315" src="https://www.youtube.com/embed/UwsrzCVZAb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-            },
-        ],
+        [{
+            title: "",
+            duration: "",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UwsrzCVZAb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        }, ],
     ],
 };
