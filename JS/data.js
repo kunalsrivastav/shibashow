38aba9692e66f842791850b8e36432b97d718779
mudit <muditshukla010@gmail.com>
2021-10-17 01:24:30 +0530

    added data for songs

--- JS/data.js
+++ JS/data.js
@@ -40,12 +40,86 @@ const data = {
             iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/diBHXLm052o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
         }
     ],
-    songs: [],
+    songs: [
+        {
+            title: "Suna Hai",
+            duration: "3:28",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/FU3bJe2TDAw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Levitating",
+            duration: "3:50",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/TUVcZfQe-Kw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+           title: "Kabhi Tumhe",
+           duration: "3:40",
+           iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/ByIZIKFmHOA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' 
+        },
+        {
+            title: "Zaroori Tha",
+            duration: "5:16",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6-n_szx2XRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Ziddi",
+            duration: "5:04",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/puKD3nkB1h4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Besabriyaan",
+            duration: "3:29",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UBBHpoW3AKA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Aashayein",
+            duration: '4:46',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/bmyv0nRkDmc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Believer",
+            duration: "3:36",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/7wtfhZwyrcc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Dusk Till Dawn",
+            duration: '5:38',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/tt2k8PGm-TI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Unstoppable",
+            duration: '3:38',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/1Pg1RguhqxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Let Me Down Slowly",
+            duration: '2:27',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/50VNCymT-Cs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Friends",
+            duration: "3:51",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/jzD_yyEcp0M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Naina Da Kya Kassor",
+            duration: "3:38",
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/zdXiSlRrgWQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Give Me Some Sunshine",
+            duration: '4:14',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/lbCRtrrMvSw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        },
+        {
+            title: "Sau Aasmaan",
+            duration: '3:51',
+            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/mpjNh-uGBY4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        }
+
+    ],
     webseries: [
-        [{
-            title: "",
-            duration: "",
-            iframeTag: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UwsrzCVZAb8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        }, ],
+
+        
     ],
 };
